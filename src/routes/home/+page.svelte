67914666720439c3360b0fc1bd173152e5b89f37
MexNowah <99980127+MexNowah@users.noreply.github.com>
2023-03-30 15:41:32 -0700

    1 file modified, 2 added, 2 deleted

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,26 @@
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Seni Home" />
+</svelte:head>
+
+<script>
+
+	let tabsOptions = [
+			{ name: "Resumen", icon: ""},
+			{ name: "Equivalentes", icon: ""},
+			{ name: "Dieta", icon: ""},
+			{ name: "Perfil", icon: ""},
+			{ name: "Noticias", icon: ""}
+	]
+
+</script>
+
+<div class="relative h-screen">
+	 	<div class="absolute w-full bottom-0 p-2 grid grid-flow-col justify-stretch">
+			{#each tabsOptions as item, i}
+				<div class="text-center">
+					{item.name}
+				</div>
+			{/each}
+		</div>
+</div>
